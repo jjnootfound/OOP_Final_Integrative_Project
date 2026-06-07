1951d2f6df8adf568881cf605b8c9728fe118d24
jayyy <a01662962@tec.mx>
2026-06-07 14:09:31 -0600

    feat:videos.h objects, constructors and class abstraction

--- videos.h
+++ videos.h
@@ -1,6 +1,41 @@
 #pragma once
+#include <string>
 
 class Videos {
+protected:
+    std::string id;
+    std::string name;
+    int length; 
+    std::string genre;
+    int rating;
+
+public:
+    //constructors for videos, normal constructor and parametrized constructor
+    Videos();
+
+    Videos(std::string newID,
+         std::string newName,
+         int newLength,
+         std::string newGenre,
+         int newRating);
+    
+    //create setters and getters
+    void setID(std::string newID);
+    void setName(std::string newName);
+    void setLength(int newLength);
+    void setGenre(std::string newGenre);
+    void setRating(int newRating);
+
+    std::string getID() const;
+    std::string getName() const;
+    std::string getGenre() const;
+    int getRating() const;
+    
+    virtual void showInfo() = 0; //abstract class
+
+    virtual ~Videos() = 0;
+    
+
 
 
 
