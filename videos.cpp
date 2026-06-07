000cad9443c6da0cc2e24ac7b3cee485889b3374
jayyy <a01662962@tec.mx>
2026-06-07 14:23:46 -0600

    feat: constructor without parametrization

--- videos.cpp
+++ videos.cpp
@@ -1,2 +1,15 @@
 #include <iostream>
 #include "videos.h"
+
+
+//normal constructor for videos, without parameters
+Videos::Videos() {
+    id = "";
+    name = "";
+    length = 0;
+    genre = "";
+    rating = 0;
+}
+
+//ahora sí, parametrized constructor for the same class
+
