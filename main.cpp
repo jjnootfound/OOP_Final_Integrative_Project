2f0c6582ef6d480f3306f6cbb70e731c66b60b2e
jayyy <a01662962@tec.mx>
2026-06-08 15:51:38 -0600

    feat: pointers in main class, declaration of null pointers

--- main.cpp
+++ main.cpp
@@ -4,14 +4,21 @@
 make pointers
 chanclas
 */
-#include <iostream>
 
+//import global libraries 
+#include <iostream>
 using namespace std;
 
+//import other classes
 #include "videos.h"
 #include "episode.h"
+#include "movie.h"
 
 int main () {
+    
+    //declare pointers for each class
+    Videos* videosArray = nullptr;
+    Episode* episodesArray = nullptr;
+    Movie* moviesArray = nullptr;
 
-    return 0;
 }
