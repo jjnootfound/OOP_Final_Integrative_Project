342d8f7682c28d966629439a978e08ce0b0d697f
jayyy <a01662962@tec.mx>
2026-06-08 18:52:12 -0600

    feat: baseline for menu in main.cpp

--- main.cpp
+++ main.cpp
@@ -22,4 +22,17 @@ int main () {
     int episodesSize = 0;
     int moviesSize = 0;
 
-}
+    //variable to make the user choose an option in the menu
+    int menuOptions;
+
+    episodesSize = countEpisodesDataLinesInCSV(SERIES_FILE);
+    moviesSize = countMoviesDataLinesInCSV(MOVIES_FILE);
+
+    //menu for user to choose what to do in the program
+    do {
+        cout<< "Media Menu: choose an option below (1-x)\n";
+        // options list
+        //todo: finish options list
+
+        cin>>menuOptions;
+    }
